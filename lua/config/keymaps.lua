0a14867eb475119575197aa40c9ee26a0a4afce1
tramoc <traluongvan@gmail.com>
2024-01-17 00:47:10 +0700

    chore: forgot to add keymap lmao

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- database
+map("n", "<leader>tt", ":DBUIToggle<CR>", { desc = "Toggle DBUI" })
+map("n", "<leader>te", ":DBUIFindBuffer<CR>", { desc = "Show current DBUI buffer in drawer" })
